69bf3d7936ef3f1ebb72a04660b59da6e435652e
Abd El Hamid <114253829+hamid-ben-hamou@users.noreply.github.com>
2023-02-04 15:26:35 +0000

    Add files via upload

---
+++ food-2/main.js
@@ -0,0 +1,9 @@
+function openFunction () {
+	document.getElementById("open").style.width = '100%';
+	document.getElementById("").style.height = 'auto';
+}
+
+function closeFunction () {
+	document.getElementById("open").style.width = '0';
+	document.getElementById("").style.height = '0';
+}
